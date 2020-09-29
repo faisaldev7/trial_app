36fd11cb20a7761e48f4f3d815e8c48f1c837503
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-09-29 13:21:25 +0530

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
+  get "users/new"
+get '/signup', :to => 'users#new'
+get '/home', :to => 'pages#home'
+get '/contact', :to => 'pages#contact'
+get '/about', :to => 'pages#about'
+get '/help', :to => 'pages#help'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
